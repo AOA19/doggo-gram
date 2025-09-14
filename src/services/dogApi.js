2b9d591aa06c516a87c5f1bb0b95a32d98ee0a14
AOA19 <aakinyemi19@gmail.com>
2025-09-14 08:41:02 -0400

    Intial commit of api

---
+++ src/services/dogApi.js
@@ -0,0 +1,21 @@
+const url = "https://api.thedogapi.com/v1";
+const apiKey = import.meta.env.VITE_DOG_API_KEY;
+
+export async function getDogImages() {
+    const res = await fetch(`${url}/images/search?limit=9&has_breeds=true`, {
+        headers: {"x-api-key": apiKey}
+    });
+
+    if (!res.ok) throw new Error("Failed to fetch dog images");
+    return res.json();
+}
+
+
+export async function getImageById(imageId) {
+    const res = await fetch(`${url}/images/${imageId}`, {
+      headers: { "x-api-key": apiKey },
+    });
+
+    if(!res.ok) throw new Error("Failed to fetch image id");
+    return res.json();
+}
