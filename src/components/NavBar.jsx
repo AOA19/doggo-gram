cbcbdf19959f0ca715334add1fc9f4e6c80917eb
AOA19 <aakinyemi19@gmail.com>
2025-09-14 08:23:31 -0400

    Updated navbar component with new header and links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,18 @@
 import {Link} from "react-router-dom"
+import { FaPaw } from "react-icons/fa";
 
 export default function NavBar() {
-    return(
-        <nav className="flex justify-end px-3">
-            <Link to="/" className="font-primary">Home</Link>
-        </nav>
-    )
+    return (
+      <nav className="flex justify-end px-3">
+        <FaPaw className="fill-electric-blue" />
+        <h1 
+            className="font-display font-bold text-7xl text-center text-rich-black">
+            DoggoGram
+        </h1>
+        <FaPaw className="fill-electric-blue" />
+        <Link to="/" className="font-primary">
+          Gallery
+        </Link>
+      </nav>
+    );
 }
